270dc0174db0a6a4716e34d43962e1b2a4d8b96c
sureshkumarreddy-k <112793655+sureshkumarreddy-k@users.noreply.github.com>
2023-05-07 23:37:06 +0530

    Create query3.py

---
+++ query3.py
@@ -0,0 +1,10 @@
+#Display all the “wooden chair” items that were manufactured before the 1st May 2023.
+import mysql.connector
+mydb = mysql.connector.connect(
+    host='localhost', user='root', password='Suresh@1702', database='INVENTORY_MANAGEMENT')
+cur = mydb.cursor()
+display='SELECT * FROM MANUFACTURE WHERE PRODUCT_NAME="WOODEN CHAIR" AND MANUFACTURE_DATE < "2023-05-01"';
+cur.execute(display);
+display1=cur.fetchall()
+for x in display1:
+    print(x)
