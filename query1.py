464ec2450951901e382ac9a25d8065b688be8be8
sureshkumarreddy-k <112793655+sureshkumarreddy-k@users.noreply.github.com>
2023-05-14 09:00:25 +0530

    Update query1.py

--- query1.py
+++ query1.py
@@ -1,4 +1,4 @@
-#Delete the defective item, e.g., the shirt which was accidentally purchased by the “ORay” store, manufactured on the date ‘01-04-23’.
+# Delete the defective item, e.g., the shirt which was accidentally purchased by the “ORay” store, manufactured on the date ‘01-04-23’.
 
 import mysql.connector;
 my_db=mysql.connector.connect(host='localhost',user='root',password='Suresh@1702',database='INVENTORY_MANAGEMENT');
