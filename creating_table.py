4559a84669f417bc787f270985aefb58cc7d1f41
sureshkumarreddy-k <112793655+sureshkumarreddy-k@users.noreply.github.com>
2023-05-14 08:57:49 +0530

    Update creating_table.py

--- creating_table.py
+++ creating_table.py
@@ -1,4 +1,4 @@
--- creating goods table
+# creating goods table
 import mysql.connector
 mydb = mysql.connector.connect(
     host='localhost', user='root', password='Suresh@1702', database='INVENTORY_MANAGEMENT')
@@ -6,7 +6,7 @@ cur = mydb.cursor()
 tb = 'CREATE TABLE GOODS(GOODS_ID INT NOT NULL, NUMBER_ITEMS INT NOT NULL, MANUFACTURE_DATE DATE NOT NULL, PRODUCT_NAME VARCHAR(20) NOT NULL,COLOR VARCHAR(20) NOT NULL, PRIMARY KEY(PRODUCT_NAME, COLOR), FOREIGN KEY(MANUFACTURE_DATE) REFERENCES MANUFACTURE(MANUFACTURE_DATE) ON DELETE CASCADE)'
 cur.execute(tb)
 
--- creating manufacture table
+# creating manufacture table
 
 import mysql.connector
 mydb = mysql.connector.connect(
@@ -15,7 +15,7 @@ cur = mydb.cursor()
 t = 'CREATE TABLE MANUFACTURE(MANUFACTURE_ID INT NOT NULL,MANUFACTURE_COMPANY VARCHAR(20),DEFECTIVE BIT NOT NULL,NUMBER_ITEMS INT NOT NULL,MANUFACTURE_DATE DATE PRIMARY KEY NOT NULL,PRODUCT_NAME VARCHAR(20) NOT NULL,COLOR VARCHAR(10) NOT NULL)'
 cur.execute(t)
 
--- creating purchase table
+# creating purchase table
 
 import mysql.connector
 my_db = mysql.connector.connect(host='localhost', user='root',
@@ -24,7 +24,7 @@ cur = my_db.cursor()
 t = 'CREATE TABLE PURCHASE(PURCHASE_ID INT PRIMARY KEY NOT NULL,STORE_MODE VARCHAR(40) NOT NULL,PRODUCT_NAME VARCHAR(20) NOT NULL,COLOR VARCHAR(10) NOT NULL,NUMBER_ITEMS INT NOT NULL,PURCHASE_DATE DATE NOT NULL,PURCHASE_AMOUNT FLOAT NOT NULL);'
 cur.execute(t)
 
--- creating sales table
+# creating sales table
 
 import mysql.connector
 mydb = mysql.connector.connect(
